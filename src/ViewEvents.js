09f5091dc28e67e4b56b7dcf16d2aa1d7a97cdcb
jon-perez18 <44533635+jon-perez18@users.noreply.github.com>
2021-04-10 04:21:35 +0000

    Added ViewEvents js and css files

---
+++ src/ViewEvents.js
@@ -0,0 +1,14 @@
+import './ViewEvents.css';
+import React from 'react';
+
+function ViewEvents(){
+    
+    return (
+        <div>
+        <h2>Events</h2>
+        <div className="box"></div>
+        </div>
+    );
+}
+
+export default ViewEvents;
