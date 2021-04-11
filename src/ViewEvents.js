1222cab9480c4639ee0ce6baecb84ecc441e5a4c
jon-perez18 <44533635+jon-perez18@users.noreply.github.com>
2021-04-11 15:31:36 +0000

    basic implementation of event card

--- src/ViewEvents.js
+++ src/ViewEvents.js
@@ -1,12 +1,33 @@
+import logo from './logo.PNG';
 import './ViewEvents.css';
 import React from 'react';
 
 function ViewEvents(){
+    // Server will send over data from db to populate page
+    // Later will use api on server retrieval?
+    const mock_data = [ {'event1':'name1'}, {'event2':'name2'} ]; // temporary, not used
+    const host = 'host';
+    const attendees = ['user 1 ','user 2'];
+    const name = 'event name';
+    const time = 'time of event';
+    const place = 'restaurant';
     
     return (
-        <div>
+        <div className="container">
         <h2>Events</h2>
-        <div className="box"></div>
+        <div className="box">
+        <div className="picture"><img className="loc-thumbnail" src={logo} alt="location picture" /></div>
+        <h4>{ name }</h4>
+        <ul>
+        <li> { host } </li>
+        <li> { time } </li>
+        <li> { place } </li>
+        <li> { attendees } </li>
+        </ul>
+        <div className="center">
+        <button type="button">Request Join</button>
+        </div>
+        </div>
         </div>
     );
 }
