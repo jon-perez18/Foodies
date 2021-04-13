8099b8eaa234d5f36c935e08affd3fd6f2558b9a
Anuja <anuja.badeti@gmail.com>
2021-04-13 01:03:32 +0000

    added frontend address input and radio buttons for radius ranges

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+      <input type="input" className="form__field" placeholder='Enter an address' />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Enter your desired range
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        
+        <input type="radio" id="one" name="range" value="5000"/>
+  <label for="one">5000 meters</label>
+  <input type="radio" id="two" name="range" value="10,000"/>
+  <label for="two">10,000 meters</label>
+  <input type="radio" id="three" name="range" value="25,000"/>
+  <label for="three">25,000 meters</label>
+  <input type="radio" id="four" name="range" value="40,000"/>
+  <label for="four">40,000 meters</label>
+  <br/>
+  <button type="button" name="input">
+          Continue
+        </button>
+        </header>
+        
     </div>
   );
 }
