981239f552206d74c8d811a5a7572b233ad38f30
mindquaker <mindquaker20@gmail.com>
2021-04-15 21:34:10 +0000

    tested

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, send_from_directory, json
-#from yelp.client import Client
+from yelp.client import Client
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 import requests
@@ -13,14 +13,31 @@ SOCKETIO = SocketIO(app,
                     json=json,
                     manage_session=False)
                     
+MY_API_KEY = os.getenv('MY_API_KEY')
+ENDPOINT='https://api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization':'bearer %s' % MY_API_KEY}
+ 
+@SOCKETIO.on('recs')
+def get_restaurant_recs(data):  # data is whatever arg you pass in your emit call on client
+    """on chat"""
+    
+    print(data)
+    PARAMS = {'term':'restaurant', 'limit': 5, 'radius': int(data['radio']), 'location': data['addy']}
 
+    response = requests.get(url=ENDPOINT, params=PARAMS, headers=HEADERS)
 
+    business_data = response.json()
+   
+    results={}
+    for i in range(5):
+        results[business_data['businesses'][i]['name']] = business_data['businesses'][i]['location']['display_address'][0]
+  
+    print(results)
+    # This emits the 'chat' event from the server to all clients except for
+    # the client that emmitted the event that triggered this function
+    SOCKETIO.emit('recs', data, broadcast=True, include_self=False)
 
-#results = {"res1":"add1","res2":"add2","res3":"add3","res4":"add4","res5":"add5"}
-####SOCKETIO.emit('recommendations', {"results":results},
-              #broadcast=True,
-              #include_self=True)
-
+    
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
