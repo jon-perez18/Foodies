a1070dab63ee83e3ea272be0a0a4e98bfca90143
Anuja <anuja.badeti@gmail.com>
2021-04-16 18:50:20 +0000

    created a new component for the search feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Search from './Search';
 import {
   React, useState, useRef, useEffect,
 } from 'react';
@@ -8,65 +9,16 @@ const socket = io(); // Connects to socket connection
 
 
 function App() {
-  const addy = useRef(null);
-  const [store_address, set_address] = useState(null);
-  const [radio,setRadio] = useState("5000");
-
-  function save_info() {
-    const input_addy = addy.current.value;
-    set_address(input_addy);
-    console.log(store_address);
-    socket.emit('recs', { addy: store_address, radio: radio });
-  }
-
+  
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div class="App">
+     <h1>Foodies App</h1>
+         <Search />
 
-      <input type="input" ref={addy} className="form__field" placeholder='Enter an address' />
-
-        <form>
-        <h3>Enter your desired range</h3>
-        
-        <input type="radio"
-        checked={radio === "5000"}
-        value="5000"
-        onChange={(e)=>{ setRadio(e.target.value)}}/>
-        <label>5000 meters</label>
-        <br/>
-        
-        <input type="radio"
-        checked={radio === "10000"}
-        value="10000"
-        onChange={(e)=>{ setRadio(e.target.value)}}/>
-        <label>10000 meters</label>
-        <br/>
-        
-        <input type="radio"
-        checked={radio === "25000"}
-        value="25000"
-        onChange={(e)=>{ setRadio(e.target.value)}}/>
-        <label>25000 meters</label>
-        <br/>
-        
-        <input type="radio"
-        checked={radio === "39000"}
-        value="39000"
-        onChange={(e)=>{ setRadio(e.target.value)}}/>
-        <label>39000 meters</label>
-        <br/>
-        <br/>
-        <button type="button" name="continue" onClick={save_info}>
-          Continue
-        </button>
-        </form>
-    
-        
-        </header>
-        
     </div>
   );
 }
 
 export default App;
+
