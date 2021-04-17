4577e2a2d2c67a2a34fcd67720756afd297964c4
mindquaker <mindquaker20@gmail.com>
2021-04-17 16:16:46 +0000

    Added functionality to add event information to the database in app.py

--- app.py
+++ app.py
@@ -6,19 +6,22 @@ load_dotenv(find_dotenv())
 import requests
 from flask_socketio import SocketIO
 from flask_cors import CORS
-from datetime import datetime
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
+from random import randint
 
 app = Flask(__name__, static_folder='./build/static')
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#db = SQLAlchemy(app)
+# Point SQLAlchemy to your Heroku database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#import models
- 
-#db.create_all()
+db = SQLAlchemy(app)
+
+import models
+
+db.create_all()
 
 SOCKETIO = SocketIO(app,
                     cors_allowed_origins="*",
@@ -81,17 +84,16 @@ def get_event_info(data):
     event_information['event_description']=event_description
     event_information['event_date']=event_date
     event_information['event_time']=event_time
+    add_event_to_db(event_information) 
    
     
-    date_time = event_date+' '+ event_time+ '%b %d %Y %I:%M%p'
-    datetime_object = datetime.strptime(date_time)
-    print(datetime_object)
-  
-    
-    
-    datetime_object = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-    
-    
+def add_event_to_db(event):
+    new_event = models.Event(host=event['host'], event_name=event['event_name'],
+    event_description=event['event_description'],event_date=event['event_date'],
+    event_time=event['event_time'])
+    db.session.add(new_event)
+    db.session.commit()
+   
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,16 +1,19 @@
-#from app import db
+from app import db
 
-#class User(db.Model):
-"""    id = db.Column(db.Integer, primary_key=True)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.name
+      
 class Event(db.Model):
-    id = db.column(db.Integer,primary_key=True)
+    event_id = db.Column(db.Integer,primary_key=True)
     host = db.Column(db.String(80),nullable=False)
-    event_name = db.Column(db.String(150),nullable=False)
-    event_description = db.Column(db.String(500), nullable=False)
-    
-"""
+    event_name = db.Column(db.String(300),nullable=False)
+    event_description = db.Column(db.String(150), nullable=False)
+    event_date = db.Column(db.String(80),nullable = False)
+    event_time = db.Column(db.String(80),nullable = False)
+    def __repr__(self):
+        return '<Event %r>' % self.event_name
