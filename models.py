ec8b8816ff121a2c11fae84abd2b71a77041e95d
mindquaker <mindquaker20@gmail.com>
2021-04-20 05:50:14 +0000

    testing

--- models.py
+++ models.py
@@ -4,11 +4,9 @@ class Login(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     name = DB.Column(DB.String(80), unique=True, nullable=False)
     email = DB.Column(DB.String(120), unique=True, nullable=False)
-
     def __repr__(self):
         return '<Login %r>' % self.name
-
-
+        
 class Event(DB.Model):
     event_id = DB.Column(DB.Integer, primary_key=True)
     host = DB.Column(DB.String(80), nullable=False)
